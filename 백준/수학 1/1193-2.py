176d9e3ddc44df70df1a81afc3dace1f30d51799
ch-4ml <ark9659@gmail.com>
2020-04-28 22:31:37 +0900

    행맨

--- 백준/수학 1/1193-2.py
+++ 백준/수학 1/1193-2.py
@@ -1,23 +1,16 @@
+x = 0
 n = int(input())
-l = 0
-r = 0
-count = 0
-limit = 2
-flag = -1
+while x * (x + 1) / 2 < n:
+    x += 1
 
-while(count < n):
-    for i in range(1, limit):
-        count += 1
-        if count == n:
-            if flag == 1:
-                l = i
-                r = limit - i
-            else:
-                l = limit - i
-                r = i
-            break
-
-    flag *= -1
-    limit += 1
-
-print(l, "/", r, sep="")
+rep = x * (x + 1) / 2 - n + 1
+for i in range(1, x + 1):
+    if x % 2 == 1:
+        l = i
+        r = x + 1 - i
+    else:
+        l = x + 1 - i
+        r = i
+    if rep == i:
+        print(l, "/", r, sep="")
+        break
