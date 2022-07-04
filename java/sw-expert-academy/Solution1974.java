0e74002a98a26bf6331f5097ed1d6ecf89aa59d8
ch-4ml <fetch@ajou.ac.kr>
2022-07-05 00:42:14 +0900

    fix: Fix SWEX-1974-Java

--- java/sw-expert-academy/Solution1974.java
+++ java/sw-expert-academy/Solution1974.java
@@ -6,7 +6,8 @@ class Solution1974
 {
 	public static void main(String args[]) throws Exception
 	{
-        final int SIZE = 9;
+        final int OUTER = 9;
+        final int INNER = 3;
         System.setIn(new FileInputStream("input/1974.txt"));
 		Scanner sc = new Scanner(System.in);
 		int T;
@@ -15,45 +16,57 @@ class Solution1974
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
             int result = 1;
-            int[][] board = new int[SIZE][SIZE];
+            int[][] board = new int[OUTER][OUTER];
 
-            rowCheck:
-            for(int i = 0; i < SIZE; i++) {
-                ArrayList<Number> row = new ArrayList<Number>();
-                for(int j = 0; j < SIZE; j++) {
-                    int current = sc.nextInt();
-                    board[i][j] = current;
-                    if(row.contains(current)) {
-                        System.out.println(row);
-                        System.out.println(current);
-                        result = 0;
-                        break rowCheck;
-                    }
-                    row.add(current);
+            // Make board
+            for(int i = 0; i < OUTER; i++) {
+                for(int j = 0; j < OUTER; j++) {
+                    board[i][j] = sc.nextInt();
                 }
             }
 
-            if(result == 1) {
-                // 세로 검사
-                colCheck:
-                for(int i = 0; i < SIZE; i++) {
-                    ArrayList<Number> col = new ArrayList<Number>();
-                    for(int j = 0; j < SIZE; j++) {
-                        int current = board[j][i];
-                        if(col.contains(current)) {
-                            System.out.println(col);
-                            System.out.println(current);
-                            result = 0;
-                            break colCheck;
+            check:
+            for(int i = 0; i < OUTER; i++) {
+                ArrayList<Number> row = new ArrayList<Number>();
+                ArrayList<Number> col = new ArrayList<Number>();
+                for(int j = 0; j < OUTER; j++) {
+                    // Check row
+                    int r = board[i][j];
+                    if(row.contains(r)) {
+                        result = 0;
+                        break check;
+                    }
+                    row.add(r);
+
+                    // Check column
+                    int c = board[j][i];
+                    if(col.contains(c)) {
+                        result = 0;
+                        break check;
+                    }
+                    col.add(c);
+                }
+            }
+
+            checkInner:
+            for(int i = 0; i < INNER; i++) {
+                for(int j = 0; j < INNER; j++) {
+                    ArrayList<Number> rect = new ArrayList<Number>();
+                    for(int k = i * INNER; k < i * INNER + INNER; k++) {
+                        for(int l = j * INNER; l < j * INNER + INNER; l++) {
+                            int r = board[k][l];
+                            if(rect.contains(r)) {
+                                result = 0;
+                                break checkInner;
+                            }
+                            rect.add(r);
                         }
-                        col.add(current);
                     }
                 }
             }
 
             System.out.println(String.format("#%s %s", test_case, result));
 		}
-		
 		sc.close();
 	}
 }
