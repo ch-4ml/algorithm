12d22b868af3649aaa99d52685fd73e23d65d782
ch-4ml <fetch@ajou.ac.kr>
2022-07-11 21:45:07 +0900

    fix: Try to fix SWEX-1247-Java (Not solved)

--- java/sw-expert-academy/Solution1247.java
+++ java/sw-expert-academy/Solution1247.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import java.io.FileInputStream;
 
@@ -23,6 +21,8 @@ class Solution1247
                 customers[i][1] = sc.nextInt();
             }
 
+            int result = getResult(work, home, customers);
+            System.out.println(String.format("#%s %s", test_case, result));
 		}
 		
 		sc.close();
@@ -46,46 +46,59 @@ class Solution1247
         }
 
         ArrayList<Integer> stack = new ArrayList<Integer>();
+        
 
         int depth = -1;
-        while(!visited[0][N-1] && stack.size() > 0) {
+        do {
             depth++;
-            int visitedIndex = -1;
-            for(int i = 0; i < N; i++) {
-                if(!visited[depth][i]) {
-                    visitedIndex = i;
-                    break;
-                }
-            }
+            int visitIndex = indexOf(visited[depth]);   
+            current[visitIndex] = true;         
+            int currentIndex = indexOf(current);
             
-            int currentIndex = -1;
-            for(int i = 0; i < N; i++) {
-                if(!current[i]) {
-                    currentIndex = i;
-                    break;
-                }
+            if(depth < N-1 && currentIndex > -1) {
+                // push, continue
+                visited[depth][visitIndex] = true;
+                current[currentIndex] = true;
+                stack.add(currentIndex);
+                continue;
             }
-
-            if(depth < N-1 && visited[depth][visitedIndex]) // 
-
-            // push 하면 continue 해야할 듯
 
             if(depth == N-1) {
                 // 거리 계산하고 result와 비교 후 작은 것으로 할당
                 int distance = 0;
-                for(int i = 1; i < N; i++) {  // 고객 사이 거리 계산
+                for(int i = 1; i < stack.size(); i++) {  // 고객 사이 거리 계산
                     int c1 = stack.get(i);
                     int c2 = stack.get(i-1);
                     distance += getDistance(customers[c1], customers[c2]);
                 }
                 distance += getDistance(work, customers[0]);  // 회사에서 첫 번째 고객 사이 거리 계산
                 distance += getDistance(customers[N-1], home);  // 마지막 고객에서 집 사이 거리 계산
+                // System.out.println(distance);
                 result = Math.min(result, distance);
             }
 
-
-            stack.remove(stack.size()-1);
-        }
+            if(indexOf(current) < 0) {
+                if(depth < N-1) {
+                    for(int i = 0; i < N; i++) {
+                        visited[depth+1][i] = false;
+                    }
+                }
+                depth--;
+                stack.remove(stack.size()-1);
+                current[visitIndex] = false;
+            }
+        } while(indexOf(visited[0]) > -1 && stack.size() > 0);
         return result;
+    }
+
+    static int indexOf(boolean[] arr) {
+        int index = -1;
+        for(int i = 0; i < arr.length; i++) {
+            if(!arr[i]) {
+                index = i;
+                break;
+            }
+        }
+        return index;
     }
 }
