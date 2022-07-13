5eff6f3ab8b2fc3eeb8b3763a352e863dc2ffc1f
ch-4ml <fetch@ajou.ac.kr>
2022-07-13 22:41:11 +0900

    feat: Add SWEA-1248-Java (not solved)

--- java/sw-expert-academy/Solution1248.java
+++ java/sw-expert-academy/Solution1248.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.io.FileInputStream;
 
@@ -7,13 +10,22 @@ class Solution1248
 	{
 		System.setIn(new FileInputStream("input/1248.txt"));
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T = sc.nextInt();
+		int T = sc.nextInt();
 
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-		
-
+			int V = sc.nextInt();
+			int E = sc.nextInt();
+			int[] sVertices = new int[E];
+			int[] fVertices = new int[E];
+			for(int i = 0; i < E; i++) {
+				sVertices[i] = sc.nextInt();
+				fVertices[i] = sc.nextInt();			
+			}
+			
+			// 이진 트리 그리기
+			
+			
 		}
 		
 		sc.close();
