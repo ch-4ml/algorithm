1450daceb0562e8bdeb764648de60d394c03ddcc
ark9659 <ark9659@gmail.com>
2022-07-15 15:38:51 +0900

    fix: Fix SWEA-1248-Java

--- java/sw-expert-academy/Solution1248.java
+++ java/sw-expert-academy/Solution1248.java
@@ -4,30 +4,50 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.io.FileInputStream;
 
-class Solution1248
-{
-	public static void main(String args[]) throws Exception
-	{
+class Solution1248 {
+	public static void main(String args[]) throws Exception {
 		System.setIn(new FileInputStream("input/1248.txt"));
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
+		for (int test_case = 1; test_case <= T; test_case++) {
 			int V = sc.nextInt();
 			int E = sc.nextInt();
-			int[] sVertices = new int[E];
-			int[] fVertices = new int[E];
-			for(int i = 0; i < E; i++) {
-				sVertices[i] = sc.nextInt();
-				fVertices[i] = sc.nextInt();			
+			ArrayList<Integer> sVertices = new ArrayList<Integer>();
+			ArrayList<Integer> fVertices = new ArrayList<Integer>();
+
+			for (int i = 0; i < E; i++) {
+				sVertices.add(sc.nextInt());
+				fVertices.add(sc.nextInt());
 			}
-			
+
 			// 이진 트리 그리기
-			
-			
+			ArrayList<Integer> binaryTree = new ArrayList<Integer>();
+			ArrayList<Integer> queue = new ArrayList<Integer>();
+			queue.add(1);
+			while (queue.size() > 0) {
+				int vertex = queue.remove(0);
+				binaryTree.add(vertex);
+				for (int i = 0; i < 2; i++) {
+					int index = indexOf(sVertices, vertex);
+					if (index > 0) {
+
+					}
+				}
+			}
 		}
-		
+
 		sc.close();
+	}
+
+	static int indexOf(ArrayList<Integer> arr, int target) {
+		int index = -1;
+		for (int i = 0; i < arr.size(); i++) {
+			if (arr.get(i) == target) {
+				index = i;
+				break;
+			}
+		}
+		return index;
 	}
 }
