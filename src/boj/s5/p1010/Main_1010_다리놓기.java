8dc28a14015c8d456bd4091b6b53a9fd91d3dd89
ch-4ml <fetch@ajou.ac.kr>
2022-08-11 00:06:37 +0900

    feat: Add BOJ-1010

--- src/boj/s5/p1010/Main_1010_다리놓기.java
+++ src/boj/s5/p1010/Main_1010_다리놓기.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 
 public class Main_1010_다리놓기 {
 	
-	static int count;
-	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -19,27 +17,35 @@ public class Main_1010_다리놓기 {
 		int T = Integer.parseInt(in.readLine());
 		int N = 0, M = 0;
 		for(int t = 0; t < T; t++) {
-			count = 0;
+			int count = 0;
 			st = new StringTokenizer(in.readLine());
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
-			combination(N, M, 0);
+
+			int[][] memo = new int[M + 1][N + 1]; 
+			for(int n = 0; n <= M; n++) {
+				for(int r = 0; r <= N; r++) {					
+					if(n == r) {
+						memo[n][r] = 1;
+						break;
+					}
+					
+					if(r == 0) {
+						memo[n][r] = 1;
+						continue;
+					}
+					
+					memo[n][r] = memo[n - 1][r - 1] + memo[n - 1][r];
+				}
+			}
+			
+			count = memo[M][N];
+			
 			sb.append(count).append("\n");
 		}
 		
 		out.write(sb.toString());
 		out.flush();
 		out.close();
-	}
-	
-	private static void combination(int r, int n, int startIndex) {
-		if(r == 0) {
-			count++;
-			return;
-		}
-		
-		for(int i = startIndex; i < n; i++) {
-			combination(r - 1, n, i + 1);
-		}
 	}
 }
