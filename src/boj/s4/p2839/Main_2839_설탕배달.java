27b8213229068b65bd9c7dc0b4d972a1ab9c1b35
ch-4ml <fetch@ajou.ac.kr>
2022-08-17 00:46:45 +0900

    feat: Add BOJ-1074

---
+++ src/boj/s4/p2839/Main_2839_설탕배달.java
@@ -0,0 +1,31 @@
+package boj.s4.p2839;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main_2839_설탕배달 {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(in.readLine());
+		
+		int x = 0, y = 0, z = N;
+		while(z > 0 && z % 5 != 0) {
+			z -= 3;
+			y++;
+		}
+		
+		if(z < 0) out.write("-1");
+		else {
+			x = z / 5;
+			out.write(String.valueOf(x + y));
+		}
+		
+		out.flush();
+		out.close();
+	}
+}
