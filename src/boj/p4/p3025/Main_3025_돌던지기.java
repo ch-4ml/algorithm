53e60080169206c0a91517411c3d51b4fa67ce00
ch-4ml <fetch@ajou.ac.kr>
2022-08-23 03:06:24 +0900

    feat: Add BOJ-3025

---
+++ src/boj/p4/p3025/Main_3025_돌던지기.java
@@ -0,0 +1,82 @@
+package boj.p4.p3025;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_3025_돌던지기 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(in.readLine());
+
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		char[][] map = new char[R + 1][C + 1];
+		
+		// 각 column으로 돌멩이가 떨어지면 굴러 떨어질 좌표를 저장해놓는 변수
+		int[][] dp = new int[C + 1][2];
+
+		for (int i = 1; i < C + 1; i++) {
+			dp[i][0] = R;
+			dp[i][1] = C;
+		}
+
+		for (int i = 1; i < R + 1; i++) {
+			String rows = in.readLine();
+			for (int j = 1; j < C + 1; j++) {
+				char c = rows.charAt(j - 1);
+				if (c == 'X') {
+					dp[j][0] = i - 1;
+					dp[j][1] = j;
+				}
+				map[i][j] = c;
+			}
+		}
+
+		int N = Integer.parseInt(in.readLine());
+
+		for (int i = 0; i < N; i++) {
+			int col = Integer.parseInt(in.readLine());
+
+			// logic
+			if(dp[col][0] > 0) map[dp[col][0]][dp[col][1]] = 'O';
+
+			// 다시 이 col으로 떨어지면 왼쪽으로 굴러떨어질 위치 저장
+			if (col > 1 && dp[col - 1][0] >= dp[col][0]) {
+				
+				// 현재 col에 대한 dp 최신화
+				dp[col][0] = dp[col - 1][0];
+				dp[col][1] = col - 1;
+				
+				// 영향을 받는 col에 대한 dp 최신화
+				dp[col - 1][0] = 
+				
+			} else if (col < R && dp[col + 1][0] >= dp[col][0]) {
+				
+				// 현재 col에 대한 dp 최신화
+				dp[col][0] = dp[col + 1][0];
+				dp[col][1] = col + 1;
+				
+			} else {
+				dp[col][0]--;
+			}
+		}
+
+		for (int i = 1; i < R + 1; i++) {
+			for (int j = 1; j < C + 1; j++) {
+				sb.append(map[i][j]);
+			}
+			sb.append("\n");
+		}
+
+		out.write(sb.toString());
+		out.flush();
+		out.close();
+	}
+}
