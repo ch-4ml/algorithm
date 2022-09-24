356a7e20e2478bad730dff2d40a767c20a588cc5
fetch <ark9659@gmail.com>
2022-09-24 13:59:47 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 188 ms, Memory: 16604 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Stack;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		Stack<Character> stack;
+		while(true) {
+			stack = new Stack<>();
+			String str = in.readLine();
+			if(str.equals(".")) break;
+			
+			boolean result = true;
+			for(int i = 0, size = str.length(); i < size; i++) {
+				Character c = str.charAt(i);
+				if(c == '(' || c == '[') {
+					stack.push(c);
+				} else if(c == ')') {
+					if(!stack.isEmpty() && stack.peek() == '(') stack.pop();
+					else {
+						result = false;
+						break;
+					}
+				} else if(c == ']') {
+					if(!stack.isEmpty() && stack.peek() == '[') stack.pop();
+					else {
+						result = false;
+						break;
+					}
+				}
+			}
+			if(stack.size() > 0) result = false;
+			sb.append(result ? "yes" : "no").append("\n");
+		}
+		out.write(sb.toString());
+		out.flush();
+		out.close();
+	}
+}
