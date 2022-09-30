394c26b3f1ad89fcd895a4082301ed0f938170d2
ark9659 <ark9659@gmail.com>
2022-09-30 15:04:55 +0900

    feat: Add solutions

--- src/boj/s3/p1463/Main_1463_1로만들기.java
+++ src/boj/s3/p1463/Main_1463_1로만들기.java
@@ -5,24 +5,25 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
 
 public class Main_1463_1로만들기 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 		
-		int N = Integer.parseInt(in.readLine());
-		out.write(String.valueOf(getResult(N)));
+		int X = Integer.parseInt(in.readLine());
+		// 정수가 X일 때 연산의 최소 횟수를 저장하는 동적 테이블
+		int[] minCounts = new int[X + 1];
+
+		for(int i = 2; i <= X; i++) {
+			minCounts[i] = minCounts[i - 1] + 1;
+			if(i % 2 == 0) minCounts[i] = Math.min(minCounts[i], minCounts[i / 2] + 1);
+			if(i % 3 == 0) minCounts[i] = Math.min(minCounts[i], minCounts[i / 3] + 1);
+		}
+
+		out.write(minCounts[X] + "");
 		out.flush();
 		out.close();
-	}
-	
-	private static int getResult(int N) {
-		int count = 0;
-		while(N > 1) {
-			
-			
-		}
-		return count;
 	}
 }
---
+++ src/boj/s4/p4949/Main_4949_균형잡힌세상.java
@@ -0,0 +1,12 @@
+package boj.s4.p4949;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+
+public class Main_4949_균형잡힌세상 {
+	public static void main(String[] args) {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(out)
+	}
+}
