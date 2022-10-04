110531eaa93197827ac75cca930e0b02e4df50fb
ark9659 <ark9659@gmail.com>
2022-10-05 08:35:11 +0900

    feat: Add BOJ-1012

--- src/boj/s2/p1012/Main_1012_유기농배추.java
+++ src/boj/s2/p1012/Main_1012_유기농배추.java
@@ -27,45 +27,42 @@ public class Main_1012_유기농배추 {
 			int N = Integer.parseInt(st.nextToken()); // row
 			int K = Integer.parseInt(st.nextToken()); // count of cabbage
 			int[][] map = new int[N][M]; // 0: 배추 x, 1: 배추, 2: 배추흰지렁이 검사 완료
-			Queue<int[]> queue = new ArrayDeque<>();
-			int[] pos = new int[2];
-			int[] curPos = new int[2];
-			int[] nextPos = new int[2];
+			Queue<int[]> queue = new ArrayDeque<>(); // 검사에 사용할 큐
+			Queue<int[]> cabbageQueue = new ArrayDeque<>(); // 양배추 좌표가 들어있는 큐
 			
 			for(int i = 0; i < K; i++) {
 				st = new StringTokenizer(in.readLine());
 				int col = Integer.parseInt(st.nextToken());
 				int row = Integer.parseInt(st.nextToken());
 				map[row][col] = 1;
+				int[] cabbage = { row, col };
+				cabbageQueue.offer(cabbage);
 			}
 			
 			int count = 0;
-			for(int i = 0; i < N; i++) {
-				for(int j = 0; j < M; j++) {
-					if(map[i][j] == 1) {						
-						pos[0] = i;
-						pos[1] = j;
-						queue.offer(pos);
-						while(!queue.isEmpty()) {;
-							curPos = queue.poll();
-							map[curPos[0]][curPos[1]] = 2;
-							for(int d = 0; d < 4; d++) {
-								int nr = curPos[0] + dr[d];
-								int nc = curPos[1] + dc[d];
-								if(nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] == 1) {
-									nextPos[0] = nr;
-									nextPos[1] = nc;
-									queue.offer(nextPos);
-								}
-							}
-							
+			while(!cabbageQueue.isEmpty()) {
+				int[] cabbage = cabbageQueue.poll();
+				if(map[cabbage[0]][cabbage[1]] == 2) continue;
+				queue.offer(cabbage);
+				while(!queue.isEmpty()) {
+					int[] c = queue.poll();
+					for(int d = 0; d < 4; d++) {
+						int nr = c[0] + dr[d];
+						int nc = c[1] + dc[d];
+						if(nr >= 0 && nr < N && nc >= 0 && nc < M && map[nr][nc] == 1) {
+							map[nr][nc] = 2;
+							int[] nextCabbage = { nr, nc };
+							queue.offer(nextCabbage);
 						}
-						count++;
 					}
 				}
+				count++;
 			}
+		
 			sb.append(count).append("\n");
 		}
+		
+		
 		out.write(sb.toString());
 		out.flush();
 		out.close();
