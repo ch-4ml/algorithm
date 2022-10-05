b7e3660c738f460990970ac8b4406abdd28f2d47
ark9659 <ark9659@gmail.com>
2022-10-05 17:42:34 +0900

    feat: Add BOJ-2636

---
+++ src/boj/g4/p2636/Main_2636_치즈.java
@@ -0,0 +1,41 @@
+package boj.g4.p2636;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main_2636_치즈 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(in.readLine());
+		int R = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+		int[][] board = new int[R][C];
+		Queue<int[]> queue = new ArrayDeque<>();
+		
+		for(int i = 0; i < R; i++) {
+			st = new StringTokenizer(in.readLine());
+			for(int j = 0; j < C; j++) {
+				int num = Integer.parseInt(st.nextToken());
+				board[i][j] = num;
+				if(num == 1) {
+					int[] pos = { i, j };
+					queue.add(pos);
+				}
+				
+			}
+		}
+		
+		
+		
+	}
+}
