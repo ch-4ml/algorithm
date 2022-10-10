975a3644f560e009835ec848ff5729cf7df9aba8
ch-4ml <ark9659@gmail.com>
2022-10-11 07:54:18 +0900

    Merge

--- src/boj/p4/p3025/Main_3025_돌던지기.java
+++ src/boj/p4/p3025/Main_3025_돌던지기.java
@@ -23,6 +23,7 @@ public class Main_3025_돌던지기 {
 	
 	static char[][] map;
 	static Node[][] direction;
+	static int R, C;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -30,20 +31,22 @@ public class Main_3025_돌던지기 {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(in.readLine());
 
-		int R = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
 
 		map = new char[R + 1][C + 1];
 		direction = new Node[R + 1][C + 1];
 
 		// Direction node 배열 초기화
+		// 가장 아래 줄
 		for (int i = 1; i < C + 1; i++) {
-			Node 
+			direction[R][i] = new Node(R, i, null);
 		}
 		
+		// 나머지 줄
 		for (int i = 1; i < C + 1; i++) {
 			for (int j = R - 1; j > 0; j--) {
-				
+				direction[j][i] = new Node(j, i, direction[j + 1][i]);
 			}
 		}
 
@@ -53,26 +56,51 @@ public class Main_3025_돌던지기 {
 			for (int j = 1; j < C + 1; j++) {
 				char status = rows.charAt(j - 1);
 				if (status == 'X') {
-					
+					direction[i - 1][j].next = null;
 				}
 				map[i][j] = status;
 			}
 		}
 
+		System.out.println(direction[2][1].next);
+		
 		// 돌 굴리기 N회
 		int N = Integer.parseInt(in.readLine());
 
+
+		System.out.println();
 		// 돌 굴리기 반복
 		for (int i = 0; i < N; i++) {
+			int col = Integer.parseInt(in.readLine());
 			
+			// 돌이 떨어질 위치 찾기
+			Node node = direction[1][col];
+			while(node.next != null) {
+				node = node.next;
+			}
+			
+			System.out.println(node.r + " " + node.c + " " + node.next);
+			
+			map[node.r][node.c] = 'O';
+
+			System.out.println();
+			for (int a = 1; a < R + 1; a++) {
+				for (int j = 1; j < C + 1; j++) {
+					System.out.print(map[a][j]);
+				}
+				System.out.println();
+			}
+			
+			// 돌이 굴러떨어질 수 있는 위치 검사
+			update(node);
 		}
 
-		for (int i = 1; i < R + 1; i++) {
-			for (int j = 1; j < C + 1; j++) {
-				sb.append(map[i][j]);
-			}
-			sb.append("\n");
-		}
+//		for (int i = 1; i < R + 1; i++) {
+//			for (int j = 1; j < C + 1; j++) {
+//				sb.append(map[i][j]);
+//			}
+//			sb.append("\n");
+//		}
 
 		out.write(sb.toString());
 		out.flush();
@@ -80,9 +108,9 @@ public class Main_3025_돌던지기 {
 	}
 	
 	public static void update(Node node) {
-		if(map[node.r][node.c - 1] == '.' && map[node.r + 1][node.c - 1] == '.') {
-			node.next = direction[node.r][node.c - 1];
-		} else if(map[node.r][node.c + 1] == '.' && map[node.r + 1][node.c + 1] == '.') {
+		if(node.r > 1 && node.c > 1 && map[node.r - 1][node.c - 1] == '.' && map[node.r][node.c - 1] == '.') {
+			node.next = direction[node.r - 1][node.c - 1];
+		} else if(node.r > 1 && node.c < C && map[node.r - 1][node.c + 1] == '.' && map[node.r][node.c + 1] == '.') {
 			node.next = direction[node.r][node.c + 1];
 		} else {
 			node.next = null;
