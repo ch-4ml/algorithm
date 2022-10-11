a00a2836cb3a69d0f3854c953b7c7f75d2dd51f7
ch-4ml <fetch@ajou.ac.kr>
2022-10-12 06:38:33 +0900

    feat: Add SWEA-4014

--- src/swea/test/p4014/Solution_4014_활주로건설.java
+++ src/swea/test/p4014/Solution_4014_활주로건설.java
@@ -8,79 +8,65 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Solution_4014_활주로건설 {
-	
-	static int N, X;
-	static int[][] map;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		int T = Integer.parseInt(in.readLine());
-		for (int t = 1; t <= T; t++) {
-			st = new StringTokenizer(in.readLine());
-			N = Integer.parseInt(st.nextToken());
-			X = Integer.parseInt(st.nextToken());
-			map = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(in.readLine());
-				for (int j = 0; j < N; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
-			}
-			
-			int answer = 0;
-			for (int i = 0; i < N; i++) {
-				answer += checkRow(i);
-				answer += checkCol(i);
-			}
-			
-			sb.append("#").append(t).append(" ").append(answer).append("\n");
-		}
-		out.write(sb.toString());
-		out.flush();
-		out.close();
-	}
-	
-	static int checkRow(int row) {
-		int count = 1;
-		boolean checkedLeft = false;
-		for (int col = 1; col < N; col++) {
-			if (map[row][col] == map[row][col - 1]) count++;
-			else if (count >= X && map[row][col] == map[row][col - 1] + 1) return 1;
-			else {
-				count = 1;
-				checkedLeft = false;
-			}
-			
-			if (!checkedLeft && count >= X) {
-				if (col - count >= 0 && map[row][col - count] == map[row][col - count + 1] + 1)
-					return 1;
-				else checkedLeft = true;
-			}
-		}
-		return 0;
-	}
-	
-	static int checkCol(int col) {
-		int count = 1;
-		boolean checkedUp = false;
-		for (int row = 1; row < N; row++) {
-			if (map[row][col] == map[row - 1][col]) count++;
-			else if (count >= X && map[row][col] == map[row - 1][col] + 1) return 1;
-			else {
-				count = 1;
-				checkedUp = false;
-			}
-			
-			if (!checkedUp && count >= X) {
-				if (row - count >= 0 && map[row - count][col] == map[row - count + 1][col] + 1)
-					return 1;
-				else checkedUp = true;
-			}
-		}
-		return 0;
-	}
+
+    static int N, X;
+    static int[][] map, map2;
+
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int T = Integer.parseInt(in.readLine());
+        for (int t = 1; t <= T; t++) {
+            st = new StringTokenizer(in.readLine());
+            N = Integer.parseInt(st.nextToken());
+            X = Integer.parseInt(st.nextToken());
+            map = new int[N][N];
+            map2 = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(in.readLine());
+                for (int j = 0; j < N; j++) {
+                    map2[j][i] = map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int answer = 0;
+            for (int i = 0; i < N; i++) {
+                answer += process(map[i]);
+                answer += process(map2[i]);
+            }
+
+            sb.append("#").append(t).append(" ").append(answer).append("\n");
+        }
+        out.write(sb.toString());
+        out.flush();
+        out.close();
+    }
+
+    static int process(int[] row) {
+        int basis = 1; // 오르막이 생길 때를 대비해서 땅의 길이를 누산
+        int count = 0; // 내리막이 생겼을 때 경사로가 놓일 수 있는지 검사하기 위한 값
+        
+        for (int col = 1; col < N; col++) {
+            if (row[col - 1] == row[col]) {
+                basis++;
+                if (count > 0) {
+                    if (--count == 0) basis = 0;
+                }
+            } else {
+                if (count > 0) return 0;
+                
+                if (row[col - 1] + 1 == row[col]) {
+                    if (basis < X) return 0;
+                }
+                else if (row[col - 1] - 1 == row[col]) count = X - 1;
+                else return 0;
+                
+                basis = 1;
+            }
+        }
+        return count > 0 ? 0 : 1;
+    }
 }
