3f7233d63b85e3ebc946484b4c3a56252f664c1c
fetch <ark9659@gmail.com>
2022-10-14 07:05:26 +0900

    [Silver III] Title: Four Squares, Time: 120 ms, Memory: 12288 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -11,23 +11,13 @@ public class Main {
 
         int N = Integer.parseInt(in.readLine()); // N <= 50000, 시간 제한이 0.5초라 빨리 계산될 수 있는 범위로 케이스를 제공하는 듯?
         int[] dt = new int[N + 1]; // 자연수 N을 제곱수들의 합으로 나타냈을 때, 구성되는 제곱수의 최소 개수
-        dt[0] = 0;
         
-        int next = 1;
-        int cur = 0;
         for (int i = 1; i <= N; i++) {
-            if (++cur == next) {
-                dt[i] = 1;
-                next += 2;
-                cur = 0;
-            } else {
-                dt[i] = dt[i - 1] + 1;
-                for (int j = 1; j <= Math.sqrt(i); j++) {
-                    dt[i] = Math.min(dt[j * j] + dt[i - j * j], dt[i]);
-                }                
+            dt[i] = dt[i - 1] + 1;
+            for (int j = 1; j <= Math.sqrt(i); j++) {
+                dt[i] = Math.min(1 + dt[i - j * j], dt[i]);
             }
         }
-        
         
         out.write(dt[N] + "");
         out.flush();
