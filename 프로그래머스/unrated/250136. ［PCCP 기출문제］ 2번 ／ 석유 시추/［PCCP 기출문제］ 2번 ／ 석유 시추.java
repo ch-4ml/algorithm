a1fabfe8604808cf6e058f0b43d037a42e41a405
Chan Park <ark9659@gmail.com>
2024-01-06 22:06:37 +0900

    [level unrated] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 21.32 ms, Memory: 65.3 MB -BaekjoonHub

--- 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/unrated/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -2,16 +2,19 @@ import java.util.*;
 
 class Solution {
     
-    static int N;
-    static int M;
-    static List<boolean[]> hasColumnByGroup;
+    int N;
+    int M;
+    boolean[][] checked;
+    List<boolean[]> hasColumnByGroup;
+    int[] dr = { -1, 0, 1, 0 };
+    int[] dc = { 0, -1, 0, 1 };
     
     public int solution(int[][] land) {
         N = land.length;
         M = land[0].length;
         hasColumnByGroup = new ArrayList();
         
-        boolean[][] checked = new boolean[N][M];
+        checked = new boolean[N][M];
         List<List<int[]>> oilGroups = new ArrayList();
         for (int i = 0; i < N; i += 1) {
             for (int j = 0; j < M; j += 1) {
@@ -40,8 +43,6 @@ class Solution {
     
     // 같은 석유 덩어리의 좌표 리스트를 반환
     private List<int[]> bfs(int[][] land, boolean[][] checked, int[] start) {
-        int[] dr = { -1, 0, 1, 0 };
-        int[] dc = { 0, -1, 0, 1 };
         boolean[] hasColumn = new boolean[M];
         List<int[]> oilList = new ArrayList();
         Queue<int[]> q = new ArrayDeque();
@@ -54,7 +55,7 @@ class Solution {
             for (int d = 0; d < 4; d += 1) {
                 int r = pos[0] + dr[d];
                 int c = pos[1] + dc[d];
-                if (isOil(land, checked, r, c)) {
+                if (isOil(land, r, c)) {
                     checked[r][c] = true;
                     int[] newPos = { r, c };
                     q.offer(newPos);
@@ -65,7 +66,7 @@ class Solution {
         return oilList;
     }
     
-    private boolean isOil(int[][] land, boolean[][] checked, int row, int col) {
+    private boolean isOil(int[][] land, int row, int col) {
         return 
             row > -1 &&
             col > -1 &&
